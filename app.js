8e5d9751f4cb1a5c2b178d2b9f15d0b8949b1bbd
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2019-05-02 19:24:38 -0700

    Intercept requests
    Block requests for everything that isn't a document.

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const puppeteer = require(`puppeteer`);
   const urls = await fs.readJson(`./data/${input}/input.json`);
   const scrapeFunc = require(`./data/${input}`);
 
+  // Enables request interception to block unwanted resources
+  await page.setRequestInterception(true);
+  page.on(`request`, req => {
+    const regex = /^(script|stylesheet|image|font|xhr|texttrack|eventsource|fetch|websocket|manifest|other)$/;
+    if (req.resourceType().match(regex)) {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+
   // Scrape for data
   let data = [];
   for (let url of urls) {
